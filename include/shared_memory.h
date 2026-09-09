a1126dcc21a9aff2332b70bcf83c11cfffd5cce2
Ashish Khetwal <155951655+ashishkhetwal@users.noreply.github.com>
2026-09-09 18:32:44 +0530

    Create Shared Memory Header File

---
+++ include/shared_memory.h
@@ -0,0 +1,30 @@
+#ifndef SHARED_MEMORY_H
+#define SHARED_MEMORY_H
+#include<cstddef> //for using size_t 
+#include<semaphore.h> //posix semaphore library
+#include<string>
+struct MemSyncHeader{
+    size_t total_bytes; //total allocated memory size
+    size_t slot_capacity; //total key value slots in cache
+    size_t active_items; //how many slots currently hold key value pairs
+};
+class SharedMemoryManager{
+    private:
+    std::string shm_name;
+    std::string sem_name;
+    size_t shm_size;
+    int shm_fd; //file descriptor
+    void* mapped_ptr; //address of the start of virtual memory
+    sem_t* semaphore; 
+    bool is_owner; //server ->true, client->false
+    public:
+    SharedMemoryManager(const std::string &shm_n, const std::string &sem_n, size_t size);
+    ~SharedMemoryManager();
+    bool init_as_server();
+    bool attach_as_client();
+    void lock();
+    void unlock();
+    void *get_base_ptr();
+    void cleanup();
+};
+#endif
